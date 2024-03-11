80bfe3a90e2abd9192993a3d8a5a5b7f1fd3083d
HimangshuDe <dehimangshu03@gmail.com>
2024-03-11 21:29:43 +0530

    Model Fields
    -> Added model fields for storing Todo Tasks and categories they are related to.

--- todo_app/models.py
+++ todo_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
+class Category(models.Model):
+    cat_name = models.CharField(unique=True, max_length=31)
+
+    def __str__(self):
+        return self.cat_name
+
+
+class Tasks(models.Model):
+    task_id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    title = models.CharField(max_length=100)
+    detail_text = models.TextField(blank=True)
+    due_date = models.DateField(blank=True, null=True)
+    is_starred = models.BooleanField(default=False)
+    is_completed = models.BooleanField(default=False)
+    cat_name = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="category"
+    )
+
+    def __str__(self):
+        return self.title
