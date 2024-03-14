219ac914e5035ba4c3f1261af648e771dd81a1f1
HimangshuDe <dehimangshu03@gmail.com>
2024-03-14 18:34:55 +0530

    Styling Home Page
    > Contind. Added styling on and finished
    
    > Added functionalities to delete, update or mark as complete a particular task.
    
    > Views and routes are updated.

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path("<slug:slug>", views.home, name="home"),
     path("create-category/", views.create_category, name="create-category"),
     path("create-task/", views.create_task, name="create-task"),
+    path("edit-task/<uuid:task_id>", views.edit_task, name="edit-task"),
+    path("is-completed/<uuid:task_id>", views.is_completed, name="is-completed"),
+    path("delete-task/<uuid:task_id>", views.delete_task, name="delete-task"),
 ]
--- todo_app/views.py
+++ todo_app/views.py
@@ -16,10 +16,19 @@ def home(request, slug=None):
     context["slug"] = None
     for items in category_data:
         # print(Category.objects.get(cat_name=items).category.all())
-        context["tasks"].extend(Category.objects.get(cat_name=items).category.all())
+        context["tasks"].extend(
+            Category.objects.get(cat_name=items).category.filter(is_completed=False)
+        )
 
-    if slug != None:
-        task_list = Category.objects.get(cat_name=slug).category.all()
+    if slug != None and slug != "completed":
+        task_list = Category.objects.get(cat_name=slug).category.filter(
+            is_completed=False
+        )
+        context["tasks"] = task_list
+        context["slug"] = slug
+
+    if slug == "completed":
+        task_list = Tasks.objects.filter(is_completed=True)
         context["tasks"] = task_list
         context["slug"] = slug
     return render(request, "todo_app/home.html", context)
@@ -44,6 +53,8 @@ def create_category(request):
     for items in category_data:
         # print(Category.objects.get(cat_name=items).category.all())
         context["tasks"].extend(Category.objects.get(cat_name=items).category.all())
+
+    # Main category part
     form = CategoryAddForm()
     context["form"] = form
     if request.method == "POST":
@@ -58,3 +69,27 @@ def create_category(request):
         "todo_app/create_category.html",
         context,
     )
+
+
+def edit_task(request, task_id):
+    task = Tasks.objects.get(task_id=task_id)
+    form = TasksAddForm(instance=task)
+    if request.method == "POST":
+        form = TasksAddForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("home"))
+    return render(request, "todo_app/create_task.html", {"form": form, "create": False})
+
+
+def is_completed(request, task_id):
+    task = Tasks.objects.get(task_id=task_id)
+    task.is_completed = not task.is_completed
+    task.save()
+    return HttpResponseRedirect(reverse("home"))
+
+
+def delete_task(request, task_id):
+    task = Tasks.objects.get(task_id=task_id)
+    task.delete()
+    return HttpResponseRedirect(reverse("home"))
