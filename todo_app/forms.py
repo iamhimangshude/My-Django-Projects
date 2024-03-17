facec052cc18ec1d960928d012f1d4275efbcb0c
HimangshuDe <dehimangshu03@gmail.com>
2024-03-17 21:09:02 +0530

    Adjusted CSS in create_task.html

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -7,6 +7,7 @@ class TasksAddForm(forms.ModelForm):
     cat_name = forms.ModelChoiceField(
         queryset=Category.objects.all(), label="Select Category", required=False
     )
+    is_starred = forms.BooleanField(label="Star Task", required=False)
 
     class Meta:
         model = Tasks
