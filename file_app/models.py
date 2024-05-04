09906006d2b867397ce9dd0d36a36741e3c9f01a
HimangshuDe <dehimangshu03@gmail.com>
2024-05-04 09:06:00 +0530

    Updated models and settings
    > Added model classes for the project
    > settings updated for handling the file uploads
    
    # Minor
    -> Migration files are present. [./file_app/migrations/]

--- file_app/models.py
+++ file_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+class Folder(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_subfolder = models.BooleanField(default=False)
+    parent_folder = models.ForeignKey(
+        "self",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="subfolders",
+    )
+
+
+class File(models.Model):
+    file = models.FileField(upload_to="files")
+    created_at = models.DateTimeField(auto_now_add=True)
+    folder = models.ForeignKey(
+        Folder,
+        on_delete=models.CASCADE,
+        related_name="files",
+        null=True,
+        blank=True,
+    )
--- file_cloud/settings.py
+++ file_cloud/settings.py
@@ -118,6 +118,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+# Media Files
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+MEDIA_URL = "/media/"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
